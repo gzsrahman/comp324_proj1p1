2958afe082ac65c67e0e1b5840245ad5fe0e479b
Gazi Rahman <gzsrahman@gmail.com>
2024-09-26 18:12:01 -0400

    Final-ish changes

--- lib/interp.ml
+++ lib/interp.ml
@@ -11,7 +11,9 @@ exception UndefinedFunction of Ast.Id.t
  *)
 exception UnboundVariable of Ast.Id.t
 
-(* DivisionByZero x1 x2 is raised when x2 = 0
+(* LambdaLegends error addition
+ *)
+(* DivisionByZero is raised when in Div e_1 e_2 e_2 = 0
  *)
 exception DivisionByZero
 
@@ -55,56 +57,68 @@ module Env = struct
 
   (* LambdaLegends work on Env starts here*)
 
-  (* we need to be able to remove existing bindings in the event
-   * that we want to assign a new value to an existing variable
-   * for example if x has value 2 but we need to update it to 
-   * have value 3.
-   *
-   * we'll do this with a recursive remove function where, for a
-   * variable x, we check if each binding pertains to a variable
-   * of the same name. if it does, then we return the rest of the
-   * list; if it doesn't, we keep the binding and check the rest.
-   * in the base case where we're checking an empty environment,
-   * we'll return an empty environment since there's nothing to
-   * remove
+  (* recursively iterate through rho list to remove old binding and keep rest
+   * necessary when updating binding for existing variable
    *)
-   let rec remove (x : Ast.Id.t) (env : t) : t =
-    match env with
-    | [] -> []
-    | (y, v) :: env' -> if x = y then env' else (y, v) :: remove x env'
+  let rec remove (rho : t) (x : Ast.Id.t) : t =
+    match rho with
+      | [] -> []
+      | (y, v) :: rho' -> if x = y then rho' else (y, v) :: remove rho' x
 
-  (* we need to be able to add bindings to the environment. in case
-   * the variable in question is already bound, we should remove the
-   * existing binding and then add the new one to the environment
-   *
-   * we'll do this by calling the remove function by default; if the
-   * variable is not bound then the remove function won't do anything.
-   * then we'll add the binding to env', which will definitely not
-   * have an existing binding for the variable, freeing us from having
-   * multiple bindings for one variable
+  (* add new variable binding to head. first remove existing binding if
+   * applicable, then add binding to the new environment
    *)
-  let add (x : Ast.Id.t) (v: Value.t) (env : t) : t =
-    let env' = remove x env in
-    (x, v) :: env'
+  let add (rho : t) (x : Ast.Id.t) (v: Value.t) : t =
+    let rho' = remove rho x in
+    (x, v) :: rho'
 
-  (* we need to be able to access the value for a given bound variable
-   *
-   * we'll just use a recursive lookup function where we look at each
-   * element in the list, if the variable name is a match, we'll return
-   * the corresponding value. if not, we'll look at the rest. we use
-   * a base case where, in the event we are either looking at an empty
-   * environment and/or we've looked through the entire list and there's
-   * nothing remaining, we'll return an UnboundVariable exception since
-   * the variable being looked up is not defined as far as we know
+  (* recursively iterate through list to find binding for input variable
+   * raise error if variable not bound
    *)
-  let rec lookup (x: Ast.Id.t) (env: t) : Value.t = 
-    match env with 
-    | [] -> raise (UnboundVariable x)
-    | (y, v) :: env' -> if x = y then v else lookup x env'
+  let rec lookup (rho: t) (x: Ast.Id.t) : Value.t = 
+    match rho with 
+      | [] -> raise (UnboundVariable x)
+      | (y, v) :: rho' -> if x = y then v else lookup rho' x
 
   (* LambdaLegends work for Env ends here *)
 
 end
+
+(* perform unary operation on a value; V_Ints can be negative and V_Bools
+ * can be logically negated
+ *)
+let unop (op : Ast.Expr.unop) (v : Value.t) : Value.t =
+  match (op,v) with 
+    |(Ast.Expr.Neg, Value.V_Int n) -> Value.V_Int(-n)
+    |(Ast.Expr.Not, Value.V_Bool b) -> Value.V_Bool(not b)
+    |_ -> raise (TypeError "Can't perform this operation on this type")
+
+(* perform binary operation on two values v and v' based on whether they
+ * are V_Ints or V_Bools; this should cover every possible operation
+ *)
+let binop (op : Ast.Expr.binop) (v : Value.t) (v' : Value.t) : Value.t =
+  match (op, v, v') with
+    | (Ast.Expr.Plus, Value.V_Int n1, Value.V_Int n2) -> Value.V_Int (n1 + n2)
+    | (Ast.Expr.Minus, Value.V_Int n1, Value.V_Int n2) -> Value.V_Int (n1 - n2)
+    | (Ast.Expr.Times, Value.V_Int n1, Value.V_Int n2) -> Value.V_Int (n1 * n2)
+    | (Ast.Expr.Div, Value.V_Int n1, Value.V_Int n2) -> 
+      if n2 = 0 then raise DivisionByZero
+      else Value.V_Int (n1 / n2)
+    | (Ast.Expr.Mod, Value.V_Int n1, Value.V_Int n2) ->
+      if n2 = 0 then raise DivisionByZero
+      else Value.V_Int (n1 mod n2)
+    | (Ast.Expr.And, Value.V_Bool b1, Value.V_Bool b2) -> Value.V_Bool (b1 && b2)
+    | (Ast.Expr.Or, Value.V_Bool b1, Value.V_Bool b2) -> Value.V_Bool (b1 || b2)
+    | (Ast.Expr.Eq, Value.V_Int n1, Value.V_Int n2) -> Value.V_Bool (n1 = n2)
+    | (Ast.Expr.Ne, Value.V_Int n1, Value.V_Int n2) -> Value.V_Bool (n1 <> n2)
+    | (Ast.Expr.Eq, Value.V_Bool b1, Value.V_Bool b2) -> Value.V_Bool (b1 = b2)
+    | (Ast.Expr.Ne, Value.V_Bool b1, Value.V_Bool b2) -> Value.V_Bool (b1 <> b2)
+    | (Ast.Expr.Lt, Value.V_Int n1, Value.V_Int n2) -> Value.V_Bool (n1 < n2)
+    | (Ast.Expr.Le, Value.V_Int n1, Value.V_Int n2) -> Value.V_Bool (n1 <= n2)
+    | (Ast.Expr.Gt, Value.V_Int n1, Value.V_Int n2) -> Value.V_Bool (n1 > n2)
+    | (Ast.Expr.Ge, Value.V_Int n1, Value.V_Int n2) -> Value.V_Bool (n1 >= n2)
+    | _ -> raise (TypeError "Invalid operands for binary operation")
+
 
 (* eval pgm ρ e = v, where pgm, ρ ├ e ↓ v.
  *
@@ -122,85 +136,59 @@ end
  *)
 let rec eval
     (pgm : Ast.Prog.fundef list)
-    (env : Env.t)
+    (rho : Env.t)
     (e : Ast.Expr.t) : Value.t =
   match e with
+
   (* LambdaLegends work starts here *)
-  (* we need to be able evaluate literals; we'll return the literals with
-   * type value
-   *)
-  | Num n -> Value.V_Int n
-  | Bool b -> Value.V_Bool b
-  (* we evaluate vars by looking them up in our environment and returning
-   * the corresponding value
-   *)
-  | Var x -> Env.lookup x env
-  (* we need to be able to evaluate every binary operation, including
-   * mathematical operations and numerical comparisons. so we need to
-   * hardcode every single operation
-   *)
-  | Binop (op, e1, e2) ->
-    (* we need to evaluate each argument in the binary operation before
-     * evaluating them in the context of the overarching program
-     *)
-    let v1 = eval pgm env e1 in
-    let v2 = eval pgm env e2 in
-    (* hardcoding every binary operation according to the appropriate op
-     *)
-    (match (op, v1, v2) with
-      | (Plus, V_Int n1, V_Int n2) -> V_Int (n1 + n2)
-      | (Minus, V_Int n1, V_Int n2) -> V_Int (n1 - n2)
-      | (Times, V_Int n1, V_Int n2) -> V_Int (n1 * n2)
-      | (Div, V_Int n1, V_Int n2) -> 
-        if n2 = 0 then raise DivisionByZero
-        else V_Int (n1 / n2)
-      | (Mod, V_Int n1, V_Int n2) ->
-        if n2 = 0 then raise DivisionByZero
-        else V_Int (n1 mod n2)
-      | (And, V_Bool b1, V_Bool b2) -> V_Bool (b1 && b2)
-      | (Or, V_Bool b1, V_Bool b2) -> V_Bool (b1 || b2)
-      | (Eq, V_Int n1, V_Int n2) -> V_Bool (n1 = n2)
-      | (Ne, V_Int n1, V_Int n2) -> V_Bool (n1 <> n2)
-      | (Eq, V_Bool b1, V_Bool b2) -> V_Bool (b1 = b2)
-      | (Ne, V_Bool b1, V_Bool b2) -> V_Bool (b1 <> b2)
-      | (Lt, V_Int n1, V_Int n2) -> V_Bool (n1 < n2)
-      | (Le, V_Int n1, V_Int n2) -> V_Bool (n1 <= n2)
-      | (Gt, V_Int n1, V_Int n2) -> V_Bool (n1 > n2)
-      | (Ge, V_Int n1, V_Int n2) -> V_Bool (n1 >= n2)
-      (* exception case incase they try to use bools for numerical
-       * comparisons or ints for conjunctions/disjunctions etc
-       *)
-      | _ -> raise (TypeError "Invalid operands for binary operation"))
-  (* need coverage of unary operations like negatives and negation
-   * i forgot if the parser would recognize -true the same way as !true
-   * so i added the exception case
-   *)
-  | Unop (op, e) ->
-    let v = eval pgm env e in
-    (match (op, v) with
-      | (Neg, V_Int n) -> V_Int (-n)
-      | (Not, V_Bool b) -> V_Bool (not b)
-      | _ -> raise (TypeError "Invalid operand for unary operation"))
-  (* need to be able to evaluate conditionals; i'm basically evaluating
-   * the antecedant and then only evaluating the consequent or alternative
-   * based on whether its true or false
-   *)
-  | If (e1, e2, e3) ->
-    let v1 = eval pgm env e1 in
+
+  (* return literals as values *)
+  | Ast.Expr.Num n -> Value.V_Int n
+  | Ast.Expr.Bool b -> Value.V_Bool b
+
+  (* return values corresponding to vars *)
+  | Ast.Expr.Var x -> Env.lookup rho x
+
+  (* for binops evaluate arguments then perform operation *)
+  | Ast.Expr.Binop (op, e1, e2) ->
+    let v1 = eval pgm rho e1 in
+    let v2 = eval pgm rho e2 in
+    binop op v1 v2
+
+  (* for unops evaluate argument then perform operation *)
+  | Ast.Expr.Unop (op, e) ->
+    let v = eval pgm rho e in 
+    unop op v 
+
+  (* for conditionals, evaluate the antecedent then evaluate and return
+   * the consequent or the alternative accordingly
+   *
+   * did not modularize because we would have to evaluate all args before
+   * calling a helper function and functional languages only evaluate
+   * the result of conditionals after evaluating the antecedent *)
+  | Ast.Expr.If (e1, e2, e3) ->
+    let v1 = eval pgm rho e1 in
     (match v1 with
-      | V_Bool true -> eval pgm env e2
-      | V_Bool false -> eval pgm env e3
+      | Value.V_Bool true -> eval pgm rho e2
+      | Value.V_Bool false -> eval pgm rho e3
       | _ -> raise (TypeError "Condition in if expression must be a boolean"))
-  (* we need let statement coverage; as in class we need to evaluate the
-   * assignment, bind it to the variable, and then evaluate the statement
+  
+  (* in a let statement, we evaluate the assignment for the variable and then
+   * the final statement; did not modularize because unnecessary here
    *)
-  | Let (x, e1, e2) ->
-    let v1 = eval pgm env e1 in
-    let env' = Env.add x v1 env in
-    eval pgm env' e2
-  (* and finally, we need function call coverage
+  | Ast.Expr.Let (x, e1, e2) ->
+    let v1 = eval pgm rho e1 in
+    let rho' = Env.add rho x v1 in
+    eval pgm rho' e2
+
+  (* for function calls, we see if the function is defined in the program; if
+   * not we throw an error. we see if the number of parameters matches the
+   * number of arguments; if not, error. finally, we evaluate all of the args
+   * by mapping the eval function to the list of arguments. then, we create a 
+   * new environment rho' where we only have the bindings for parameter to the
+   * value of its corresponding argument. we evaluate under rho'.
    *)
-  | Call (Var f, args) ->
+  | Ast.Expr.Call (Ast.Expr.Var f, args) ->
     let func = 
       try 
         List.find (fun (Ast.Prog.FunDef (name, _, _)) -> name = f) pgm
@@ -211,11 +199,18 @@ let rec eval
         if List.length params <> List.length args then
           raise (TypeError "Incorrect number of arguments in function call")
         else
-          let arg_vals = List.map (eval pgm env) args in
-          let env' = List.fold_left2 (fun env param arg_val -> Env.add param arg_val env) env params arg_vals in
-          eval pgm env' body
-      | exception Not_found -> raise (UndefinedFunction f))
+          let arg_vals = List.map (eval pgm rho) args in
+          (* List.fold_left2 basically iterates through two lists, param and
+           * arg_val, and cumulatively adds them to rho' by binding each
+           * param to the value of the corresponding argument. confusing, ik *)
+          let rho' = List.fold_left2 (fun rho param arg_val -> Env.add rho param arg_val) rho params arg_vals in
+          eval pgm rho' body)
+    
+  (* we should have defined ever possible eval case by now so if none of the 
+   * cases match, throw an error *)
   | _ -> raise (TypeError "Unsupported expression")
+
+  (* LambdaLegends work ends here*)
 
 (* exec p = v, where `v` is the result of executing `p`.
  *)
